236818a6fc39177ac9e153b8dd2169ddeb978db0
Bill Tomczak <bill@ostraining.com>
2015-05-21 17:36:42 -0700

    Refactor rest classes

--- ostoolbar/library/ostoolbar/Rest/Request.php
+++ ostoolbar/library/ostoolbar/Rest/Request.php
@@ -10,57 +10,76 @@ namespace Ostoolbar\Rest;
 defined( 'ABSPATH' ) or die();
 
 class Request {
+	/**
+	 * @param string       $url
+	 * @param string|array $data
+	 * @param string       $method
+	 * @param array        $curl_options
+	 *
+	 * @return Response
+	 */
 	public static function send( $url, $data = null, $method = 'GET', $curl_options = array() ) {
 		$data   = self::prepareData( $data );
 		$handle = curl_init();
 
 		self::setCurlOption( $handle, CURLOPT_RETURNTRANSFER, true, $curl_options );
 
-		if ( $method == 'POST' ) :
-			curl_setopt( $handle, CURLOPT_POST, true );
-			if ( $data ) :
-				curl_setopt( $handle, CURLOPT_POSTFIELDS, $data );
-			endif;
-		elseif ( $method == 'GET' ) :
-			if ( strpos( $url, "?" ) !== false ) :
-				$url .= "&" . $data;
-			else :
-				$url .= "?" . $data;
-			endif;
-		endif;
+		switch ( $method ) {
+			case 'POST':
+				curl_setopt( $handle, CURLOPT_POST, true );
+				if ( $data ) {
+					curl_setopt( $handle, CURLOPT_POSTFIELDS, $data );
+				}
+				break;
 
+			case 'GET':
+				$divider = strpos( $url, '?' ) === false ? '?' : '&';
+				$url .= $divider . $data;
+				break;
+		}
 		curl_setopt( $handle, CURLOPT_URL, $url );
 
-		if ( ! empty( $curl_options ) ) :
-			foreach ( $curl_options as $option => $value ) :
+		if ( ! empty( $curl_options ) ) {
+			foreach ( $curl_options as $option => $value ) {
 				curl_setopt( $handle, $option, $value );
-			endforeach;
-		endif;
+			}
+		}
 
 		$receive_headers = isset( $curl_options[ CURLOPT_HEADER ] );
-		$response        = new RestResponse( $handle, $receive_headers );
+		$response        = new Response( $handle, $receive_headers );
 
 		return $response;
 	}
 
-	private static function setCurlOption( &$handle, $name, $value, $curl_options = array() ) {
+	/**
+	 * @param resource $handle
+	 * @param int      $name
+	 * @param mixed    $value
+	 * @param array    $curl_options
+	 *
+	 * @return bool
+	 */
+	protected static function setCurlOption( $handle, $name, $value, $curl_options = array() ) {
 		$options = array_keys( $curl_options );
-		if ( in_array( $name, $options ) ) :
+		if ( in_array( $name, $options ) ) {
 			return false;
-		else :
+		} else {
 			curl_setopt( $handle, $name, $value );
 
 			return true;
-		endif;
+		}
 	}
 
-	private static function prepareData( $data ) {
-		if ( is_array( $data ) ) :
+	/**
+	 * @param string|array $data
+	 *
+	 * @return string
+	 */
+	protected static function prepareData( $data ) {
+		if ( is_array( $data ) ) {
 			$data = http_build_query( $data );
-		endif;
+		}
 
 		return $data;
 	}
-
 }
-
--- ostoolbar/library/ostoolbar/Rest/Response.php
+++ ostoolbar/library/ostoolbar/Rest/Response.php
@@ -11,24 +11,91 @@ namespace Ostoolbar\Rest;
 defined( 'ABSPATH' ) or die();
 
 class Response {
+	/**
+	 * @var array
+	 */
 	protected $headers = array();
+
+	/**
+	 * @var string
+	 */
 	protected $body = null;
-	protected $info = null;
+
+	/**
+	 * @var array
+	 */
+	protected $info = array();
+
+	/**
+	 * @var Error
+	 */
 	protected $error = null;
 
-	public function __construct( &$handle, $receive_headers ) {
+	protected static $http_codes = Array(
+		100 => 'Continue',
+		101 => 'Switching Protocols',
+		200 => 'OK',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found',
+		303 => 'See Other',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		306 => '(Unused)',
+		307 => 'Temporary Redirect',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported'
+	);
+
+	public function __construct( $handle, $receive_headers ) {
+		$this->error = new Error();
+
 		$this->processRequest( $handle, $receive_headers );
 	}
 
-	protected function processRequest( &$handle, $receive_headers ) {
+	/**
+	 * @param resource $handle
+	 * @param bool     $receive_headers
+	 *
+	 * @return void
+	 */
+	protected function processRequest( $handle, $receive_headers ) {
 		$response = curl_exec( $handle );
 
-		if ( $receive_headers ) :
+		if ( $receive_headers ) {
 			list( $headers, $body ) = explode( "\r\n\r\n", $response, 2 );
 			$this->setHeaders( $headers );
-		else :
+		} else {
 			$body = $response;
-		endif;
+		}
 
 		$this->setBody( $body );
 		$this->setInfo( curl_getinfo( $handle ) );
@@ -36,138 +103,138 @@ class Response {
 		curl_close( $handle );
 	}
 
-	public function setErrors( &$handle ) {
-		$this->error          = new stdClass();
-		$this->error->code    = null;
-		$this->error->message = null;
+	/**
+	 * @param resource $handle
+	 *
+	 * @return void
+	 */
+	public function setErrors( $handle ) {
+		$this->error = new Error();
 
-		if ( curl_errno( $handle ) ) :
-			$this->error->code    = curl_errno( $handle );
+		if ( $error_code = curl_errno( $handle ) ) {
+			$this->error->code    = $error_code;
 			$this->error->message = curl_error( $handle );
 
-			return;
-		endif;
-
-		$code = $this->getInfo( 'http_code' );
-		if ( $code >= 400 && $code < 600 ) :
-			$this->error->code    = $code;
-			$this->error->message = $this->getStatusMessage( $code );
-		endif;
-
+		} else {
+			$code = $this->getInfo( 'http_code' );
+			if ( $code >= 400 && $code < 600 ) {
+				$this->error->code    = $code;
+				$this->error->message = $this->getStatusMessage( $code );
+			}
+		}
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function hasError() {
-		if ( $this->error->code ) :
-			return true;
-		else :
-			return false;
-		endif;
+		return ( $this->error && $this->error->code );
 	}
 
+	/**
+	 * @return int
+	 */
 	public function getErrorCode() {
 		return $this->error->code;
 	}
 
+	/**
+	 * @param int $code
+	 *
+	 * @return int
+	 */
 	public function setErrorCode( $code ) {
 		$this->error->code = $code;
 
 		return $this->error->code;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getErrorMsg() {
 		return $this->error->message;
 	}
 
+	/**
+	 * @param string $msg
+	 *
+	 * @return string
+	 */
 	public function setErrorMsg( $msg ) {
 		$this->error->message = $msg;
 
 		return $this->error->message;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getBody() {
 		return $this->body;
 	}
 
+	/**
+	 * @param string $body
+	 *
+	 * @return string
+	 */
 	public function setBody( $body ) {
 		$this->body = $body;
 
 		return $this->body;
 	}
 
+	/**
+	 * @param array $info
+	 *
+	 * @return array
+	 */
 	protected function setInfo( $info ) {
 		$this->info = $info;
 
 		return $this->info;
 	}
 
-	protected function setHeaders( $header_string ) {
-		$headers       = explode( "\r\n", $header_string );
-		$this->headers = $headers;
+	/**
+	 * @param string $headers
+	 *
+	 * @return array
+	 */
+	protected function setHeaders( $headers ) {
+		$this->headers = explode( "\r\n", $headers );
 
 		return $this->headers;
 	}
 
+	/**
+	 * @param string $key
+	 *
+	 * @return mixed
+	 */
 	public function getInfo( $key = null ) {
-		if ( $key ) :
-			if ( isset( $this->info[ $key ] ) ) :
+		if ( $key ) {
+			if ( isset( $this->info[ $key ] ) ) {
 				return $this->info[ $key ];
-			else :
-				return null;
-			endif;
-		else :
+			}
+		} else {
 			return $this->info;
-		endif;
+		}
+
+		return null;
 	}
 
+	/**
+	 * @param $status
+	 *
+	 * @return null|string
+	 */
 	public static function getStatusMessage( $status ) {
-		$codes = Array(
-			100 => 'Continue',
-			101 => 'Switching Protocols',
-			200 => 'OK',
-			201 => 'Created',
-			202 => 'Accepted',
-			203 => 'Non-Authoritative Information',
-			204 => 'No Content',
-			205 => 'Reset Content',
-			206 => 'Partial Content',
-			300 => 'Multiple Choices',
-			301 => 'Moved Permanently',
-			302 => 'Found',
-			303 => 'See Other',
-			304 => 'Not Modified',
-			305 => 'Use Proxy',
-			306 => '(Unused)',
-			307 => 'Temporary Redirect',
-			400 => 'Bad Request',
-			401 => 'Unauthorized',
-			402 => 'Payment Required',
-			403 => 'Forbidden',
-			404 => 'Not Found',
-			405 => 'Method Not Allowed',
-			406 => 'Not Acceptable',
-			407 => 'Proxy Authentication Required',
-			408 => 'Request Timeout',
-			409 => 'Conflict',
-			410 => 'Gone',
-			411 => 'Length Required',
-			412 => 'Precondition Failed',
-			413 => 'Request Entity Too Large',
-			414 => 'Request-URI Too Long',
-			415 => 'Unsupported Media Type',
-			416 => 'Requested Range Not Satisfiable',
-			417 => 'Expectation Failed',
-			500 => 'Internal Server Error',
-			501 => 'Not Implemented',
-			502 => 'Bad Gateway',
-			503 => 'Service Unavailable',
-			504 => 'Gateway Timeout',
-			505 => 'HTTP Version Not Supported'
-		);
-		if ( isset( $codes[ $status ] ) ) :
-			return $codes[ $status ];
-		else :
-			return null;
-		endif;
+		if ( isset( static::$http_codes[ $status ] ) ) {
+			return static::$http_codes[ $status ];
+		}
+
+		return null;
 	}
 
 }
